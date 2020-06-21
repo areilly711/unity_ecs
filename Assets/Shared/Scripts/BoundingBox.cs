6d6ae39a09001acb670f1d68908e0c52a8d75ece
Adam Reilly <areilly711@yahoo.com>
2020-06-21 15:03:49 -0700

    re-organizing multiple unity projects into one

---
+++ Assets/Shared/Scripts/BoundingBox.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[System.Serializable]
+public struct BoundingBox : IComponentData
+{
+    public Bounds box;
+    public AABB aabb;
+}
--- GameOfLife/Assets/Scripts/ECS/BoundingBox.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using Unity.Entities;
-
-[System.Serializable]
-public struct BoundingBox : IComponentData
-{
-    public Bounds box;
-}
